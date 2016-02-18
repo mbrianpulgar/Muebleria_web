1ec2317ac44cef1d54807f3dd768687b999c73d2
Michael Brian Pulgar Olguín <m.brianpulgar@gmail.com>
2016-02-18 14:09:29 -0300

    Revert "Revert "Muebleria_web""
    This reverts commit 586fcadb9d31ce92a7bc2094e036a93ce93750c2.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Muebleria_web
+﻿# Muebleria_web
+Trabajo de Universidad, cliente real
+Realizado por Michael Brian Pulgar Olguin, ing civil en computación
 Sitio web para la Muebleria Véliz
---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).on('ready',function(){
+	init();
+});
+
+function init(){
+	$('.menu').localScroll();
+	$('.galeria').localScroll();
+	$('.estar').localScroll();
+	$('.link').localScroll();
+
+}
